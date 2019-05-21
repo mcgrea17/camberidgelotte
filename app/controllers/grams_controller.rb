dff124956b007bef91de53babc0b3edacfc5c8c8
Amber McGregor <amber@vqhomes.com>
2019-05-21 00:00:30 +0000

    Added Grams to the Database

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,25 @@
 class GramsController < ApplicationController
     def index
     end
+
+    def new
+        
+    end
+
+    def create
+        @gram = Gram.create(gram_params)
+
+        if @gram.valid?
+             redirect_to root_path  
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def gram_params
+        params.require(:gram).permit( :message)
+    end
+
 end
